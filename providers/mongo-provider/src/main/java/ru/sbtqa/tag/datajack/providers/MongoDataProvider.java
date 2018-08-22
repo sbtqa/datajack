cd115a17bfa356ee5ca1d44f707cd872cde274cc
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2018-08-23 02:15:22 +0700

    Mongo ref (#21)
    * New adaptor
    
    * Refactoring seems done
    
    * All test are passing

--- providers/mongo-provider/src/main/java/ru/sbtqa/tag/datajack/providers/MongoDataProvider.java
+++ providers/mongo-provider/src/main/java/ru/sbtqa/tag/datajack/providers/MongoDataProvider.java
@@ -14,43 +14,84 @@ import ru.sbtqa.tag.datajack.exceptions.*;
 import static com.mongodb.BasicDBObject.parse;
 import static com.mongodb.QueryBuilder.start;
 import static java.lang.String.format;
+import static org.apache.commons.io.FileUtils.readFileToString;
 
 public class MongoDataProvider extends AbstractDataProvider {
 
     private static final Logger LOG = LoggerFactory.getLogger(MongoDataProvider.class);
+    protected String collectionName;
     private static final String REF_ID_TPL = "refId";
     private final DB db;
-    private DBCollection coll;
+    private DBCollection collection;
+
 
     /**
      * Initialize new data object
      *
-     * @param db   Database object
-     * @param coll Collection
+     * @param db Database object
      * @throws DataException if no collection or its empty
      */
-    public MongoDataProvider(DB db, String coll) throws DataException {
+    public MongoDataProvider(DB db, String collectionName) throws DataException {
         this.db = db;
-        this.coll = this.db.getCollection(coll);
-        if (this.coll.count() == 0) {
-            throw new CollectionNotfoundException(String.format("There is no \"%s\" collection or it's empty", coll));
+        this.collection = this.db.getCollection(collectionName);
+        if (this.collection.count() == 0) {
+            throw new CollectionNotfoundException(String.format("There is no \"%s\" collection or it's empty", collectionName));
         }
-        this.way = coll;
+        this.basicObj = (BasicDBObject) collection.find().sort(new BasicDBObject("_id",-1)).limit(1).next();
+        this.way = collectionName;
+        this.path = collectionName;
+        this.collectionName = collectionName;
 
     }
 
-    private MongoDataProvider(DB db, BasicDBObject obj, String way) {
+    /**
+     * Initialize new data object
+     *
+     * @param db Database object
+     * @throws DataException if no collection or its empty
+     */
+    public MongoDataProvider(DB db, String collectionName, String refId) throws DataException {
+        this.db = db;
+        this.collection = this.db.getCollection(collectionName);
+        if (this.collection.count() == 0) {
+            throw new CollectionNotfoundException(String.format("There is no \"%s\" collection or it's empty", collectionName));
+        }
+        this.basicObj = (BasicDBObject) this.collection.findOne(new BasicDBObject("_id", new ObjectId(refId)));
+        this.way = collectionName;
+        this.collectionName = collectionName;
+        this.path="";
+
+    }
+
+    /**
+     * Internal use only
+     *
+     * @param obj            basic object
+     * @param collectionName file name
+     * @param way            complex path to value
+     */
+    protected MongoDataProvider(DB db, BasicDBObject obj, String collectionName, String way) {
         this.db = db;
         this.basicObj = obj;
         this.way = way;
+        this.collectionName = collectionName;
     }
 
+    public BasicDBObject getBasicDBObject() {
+        return this.basicObj;
+    }
+
+    public static boolean isArray(String key) {
+        return key.matches("(.+\\[\\d+\\])");
+    }
+
+
     @Override
-    public MongoDataProvider fromCollection(String collectionName) throws DataException {
-        MongoDataProvider dataProvider = new MongoDataProvider(this.db, collectionName);
+    public MongoDataProvider fromCollection(String collName) throws DataException {
+
+        MongoDataProvider dataProvider = new MongoDataProvider(this.db, collName);
         dataProvider.applyGenerator(this.callback);
         return dataProvider;
-
     }
 
     /**
@@ -60,9 +101,16 @@ public class MongoDataProvider extends AbstractDataProvider {
      * @throws DataException if no collection or its empty
      */
     public MongoDataProvider fromCollection(String collectionName, String refId) throws DataException {
-        this.coll = db.getCollection(collectionName);
-        DBObject referenceDocument = this.coll.findOne(new BasicDBObject("_id", new ObjectId(refId)));
-        MongoDataProvider dataProvider = new MongoDataProvider(this.db, collectionName);
+        this.collection = db.getCollection(collectionName);
+
+        ObjectId id= new ObjectId(refId);
+        BasicDBObject obj = new BasicDBObject();
+        obj.append("_id", id);
+        BasicDBObject query = new BasicDBObject();
+        query.putAll((BSONObject)query);
+        DBObject referenceDocument = collection.findOne(query);
+
+        MongoDataProvider dataProvider = new MongoDataProvider(this.db, collectionName, refId);
         dataProvider.basicObj = (BasicDBObject) referenceDocument;
         dataProvider.path = refId + "." + collectionName;
         dataProvider.applyGenerator(this.callback);
@@ -71,68 +119,102 @@ public class MongoDataProvider extends AbstractDataProvider {
     }
 
     @Override
-    public MongoDataProvider get(String key) throws DataException {
+    public TestDataProvider get(String key) throws DataException {
         if (key.isEmpty()) {
             return this;
         }
         this.way = key;
-        String documentId = "HERE IS MUST BE ID";
-        MongoDataProvider dataProvider;
-        if (this.basicObj == null) {
+        return key.contains(".") ? getComplex(key) : getSimple(key);
 
-            try (DBCursor cursor = coll.find(start(key).exists(true).get()).limit(1).sort((DBObject) parse("{$natural:-1}"))) {
-                if (cursor.hasNext()) {
-                    this.basicObj = (BasicDBObject) cursor.next();
-                    documentId = this.basicObj.getString("_id");
-                } else {
-                    throw new FieldNotFoundException(format("Collection \"%s\" doesn't contain \"%s\" field on path \"%s\"",
-                            this.coll.getName(), key.split("[.]")[key.split("[.]").length - 1], key));
-                }
-            }
-        }
-        if (key.contains(".")) {
-            String[] keys = key.split("[.]");
-            BasicDBObject basicO = this.basicObj;
-            for (String partialKey : keys) {
-                if (!(basicO.get(partialKey) instanceof BasicDBObject)) {
-                    break;
-                }
-                basicO = (BasicDBObject) basicO.get(partialKey);
-            }
+    }
 
-            dataProvider = new MongoDataProvider(this.db, basicO, this.way);
+    private TestDataProvider getSimple(String key) throws DataException {
+        Object result;
 
-            if (this.path == null || (this.coll != null && this.path.equals(this.coll.getName()))) {
-                this.path = documentId + "." + this.coll.getName();
-            } else if (this.coll == null) {
-                this.path = documentId + "." + this.path;
+        if (isArray(key)) {
+            result = parseArray(basicObj, key);
+        } else {
+
+            if (!basicObj.containsField(key)) {
+                throw new FieldNotFoundException(format("Collection \"%s\" doesn't contain \"%s\" field in path \"%s\"",
+                        this.collectionName, key, this.path));
             }
-            dataProvider.applyGenerator(this.callback);
-            dataProvider.coll = coll;
-            dataProvider.setRootObj(this.rootObj, this.path + "." + key);
-            return dataProvider;
+            result = this.basicObj.get(key);
         }
-        if (!basicObj.containsField(key)) {
-            throw new FieldNotFoundException(format("Collection \"%s\" doesn't contain \"%s\" field in path \"%s\"",
-                    this.coll.getName(), key, this.path));
-        }
-        Object result = basicObj.get(key);
         if (!(result instanceof BasicDBObject)) {
             result = new BasicDBObject(key, result);
         }
-
-        dataProvider = new MongoDataProvider(this.db, (BasicDBObject) result, this.way);
+        MongoDataProvider dataProvider = new MongoDataProvider(db, (BasicDBObject) result, this.collectionName, this.way);
+        dataProvider.applyGenerator(this.callback);
 
         String rootObjValue;
         if (this.path != null) {
             rootObjValue = this.path + "." + key;
         } else {
-            rootObjValue = this.coll.getName() + "." + key;
+            rootObjValue = this.collectionName + "." + key;
         }
-        dataProvider.applyGenerator(this.callback);
-        dataProvider.coll = coll;
         dataProvider.setRootObj(this.rootObj, rootObjValue);
+        if (dataProvider.isReference()) {
+            return dataProvider.getReference();
+        }
         return dataProvider;
+    }
+
+    private TestDataProvider getComplex(String key) throws DataException {
+
+        MongoDataProvider dataProvider = new MongoDataProvider(db, parseComplexDBObject(key), this.collectionName, this.way);
+        dataProvider.applyGenerator(this.callback);
+        dataProvider.setRootObj(this.rootObj, this.collectionName + "." + key);
+
+        return dataProvider;
+    }
+
+    private BasicDBObject parseComplexDBObject(String key) throws DataException {
+        String[] keys = key.split("[.]");
+        StringBuilder partialBuilt = new StringBuilder();
+        BasicDBObject basicObject = this.basicObj;
+
+        for (String partialKey : keys) {
+            partialBuilt.append(partialKey);
+
+            if (isArray(partialKey)) {
+                basicObject = (BasicDBObject) parseArray(basicObject, partialKey);
+                continue;
+            }
+
+            if (isReference(basicObject)) {
+                String collection = ((BasicDBObject) basicObject.get("value")).getString("collection");
+                String path = ((BasicDBObject) basicObject.get("value")).getString("path");
+                TestDataProvider dataProvider = new MongoDataProvider(db, collection).get(path);
+                basicObject = ((MongoDataProvider) dataProvider).basicObj;
+            }
+
+            if (!(basicObject.get(partialKey) instanceof BasicDBObject)) {
+                if (null == basicObject.get(partialKey)) {
+                    throw new FieldNotFoundException(format("Collection \"%s\" doesn't contain \"%s\" field on path \"%s\"",
+                            this.collectionName, partialKey, partialBuilt.toString()));
+                }
+                break;
+            }
+            basicObject = (BasicDBObject) basicObject.get(partialKey);
+            partialBuilt.append(".");
+        }
+        return basicObject;
+    }
+
+    private Object parseArray(BasicDBObject basicO, String key) throws DataException {
+        if (!isArray(key)) {
+            throw new DataException(String.format("%s.%s is not an array!", this.collectionName, key));
+        }
+        String arrayKey = key.split("\\[")[0];
+        String arrayIndex = key.split("\\[")[1].split("\\]")[0];
+        Object listCandidate = basicO.get(arrayKey);
+
+        if (!(listCandidate instanceof BasicDBList)) {
+            throw new DataException(String.format("%s.%s is not an array!", this.collectionName, key));
+        }
+
+        return ((BasicDBList) listCandidate).get(arrayIndex);
     }
 
     @Override
@@ -144,17 +226,8 @@ public class MongoDataProvider extends AbstractDataProvider {
     }
 
     private void setRootObj(BasicDBObject obj, String path) {
-        if (obj != null && obj.containsField(VALUE_TPL)
-                && ((BasicDBObject) obj.get(VALUE_TPL)).containsField(REF_ID_TPL)) {
-            String colName = "";
-            if (this.coll != null) {
-                colName = "." + this.coll.getName();
-            }
-            this.path = ((BasicBSONObject) obj.get(VALUE_TPL)).getString(REF_ID_TPL) + colName + "." + this.way;
-        } else {
-            this.path = path;
-        }
         this.rootObj = obj;
+        this.path = path;
     }
 
     @Override
@@ -165,12 +238,11 @@ public class MongoDataProvider extends AbstractDataProvider {
             LOG.debug("Reference not found", e);
             String result = this.basicObj.getString(VALUE_TPL);
             if (result == null) {
-                if (this.way.contains(".")) {
+                if (this.way != null && this.way.contains(".")) {
                     this.way = this.way.split("[.]")[this.way.split("[.]").length - 1];
                 }
                 result = this.basicObj.getString(this.way);
             }
-
             if (this.callback != null) {
                 CallbackData generatorParams = new CallbackData(this.path, result);
                 Object callbackResult = null;
@@ -205,19 +277,12 @@ public class MongoDataProvider extends AbstractDataProvider {
             }
             String referencedCollection = ((BasicBSONObject) this.basicObj.get(VALUE_TPL)).getString(COLLECTION_TPL);
             this.path = ((BasicBSONObject) this.basicObj.get(VALUE_TPL)).getString("path");
-            MongoDataProvider reference;
-            if (((BSONObject) this.basicObj.get(VALUE_TPL)).containsField(REF_ID_TPL)) {
-                String refId = ((BasicBSONObject) this.basicObj.get(VALUE_TPL)).getString(REF_ID_TPL);
-                reference = this.fromCollection(referencedCollection, refId);
-            } else {
-                reference = this.fromCollection(referencedCollection);
-            }
-
-            reference.setRootObj(this.rootObj, referencedCollection);
+            MongoDataProvider reference = this.fromCollection(((BasicBSONObject) this.basicObj.get(VALUE_TPL)).getString(COLLECTION_TPL));
+            reference.setRootObj(this.rootObj, referencedCollection + "." + this.path);
             return reference.get(this.path);
         } else {
             throw new ReferenceException(String.format("There is no reference in \"%s\". Collection \"%s\"",
-                    this.path, this.coll.getName()));
+                    this.path, this.collectionName));
         }
     }
 
@@ -228,10 +293,18 @@ public class MongoDataProvider extends AbstractDataProvider {
 
     @Override
     public boolean isReference() throws DataException {
-        Object value = this.basicObj.get(VALUE_TPL);
+        Object value = this.basicObj.get("value");
         if (!(value instanceof BasicDBObject)) {
             return false;
         }
-        return ((BasicDBObject) value).containsField(COLLECTION_TPL) && ((BasicDBObject) value).containsField("path");
+        return ((BasicDBObject) value).containsField("collection") && ((BasicDBObject) value).containsField("path");
+    }
+
+    public boolean isReference(BasicDBObject basicDBObject) throws DataException {
+        Object value = basicDBObject.get("value");
+        if (!(value instanceof BasicDBObject)) {
+            return false;
+        }
+        return ((BasicDBObject) value).containsField("collection") && ((BasicDBObject) value).containsField("path");
     }
 }
